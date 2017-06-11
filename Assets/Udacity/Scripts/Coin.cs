3d5400934eb0cefe0e4c1c9f49e9f3def1a9fe31
Krzysztof Barczynski <krzysztof.barczynski@nokia-bell-labs.com>
2017-06-10 21:01:45 -0700

    added extra coins. v2

--- Assets/Udacity/Scripts/Coin.cs
+++ Assets/Udacity/Scripts/Coin.cs
@@ -11,10 +11,10 @@ public class Coin : MonoBehaviour
 
     private Vector3[] randomPosition = new Vector3[]{ new Vector3(25.47f, 1.208f, 85.36f), new Vector3(16.4f, 1.208f, 89.66f), new Vector3(8.54f, 1.208f, 89.66f), new Vector3(10.58f, 1.208f, 77.81f), new Vector3(-7, 0, -8.4f), new Vector3(16.38f, 1.35f, 90.2f)}; 
     public void Start() {
-        Vector3 intialPosition = randomPosition[0];   //  randomPosition[Random.Range(0, randomPosition.Length)]; - it's hard to test with real random position
+        /* Vector3 intialPosition =   randomPosition[Random.Range(0, randomPosition.Length)]; - it's hard to test with real random position
         coin.transform.position = intialPosition;
 
-        Debug.Log(intialPosition);
+        Debug.Log(intialPosition); */
     }
 
     public void OnCoinClicked() {
---
+++ README.md
@@ -0,0 +1,8 @@
+Unity 5.5.1f1
+Google VR Version 1.0 
+Build/Tested on Android (Google Pixel. Android version 7.0)
+
+
+# 2.0
+Added extra coins
+
